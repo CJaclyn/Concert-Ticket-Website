57c020605921b7a4fd110a16218f6b7277963e9e
Jaclyn <44209123+JaclynCao@users.noreply.github.com>
2019-11-22 15:06:28 -0600

    minor change - removed nav for logout admin

--- admin/adminlogout.php
+++ admin/adminlogout.php
@@ -18,23 +18,6 @@
 </head>
 <body>
 <?php include('../header.html');?>
-  <nav>
-    <ul>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="concerts.php">Concerts<i class="down"></i></a>
-        <ul>
-          <li><a href="pop.php">Pop</a></li>
-          <li><a href="rock.php">Rock</a></li>
-          <li><a href="edm.php">EDM</a></li>
-          <li><a href="metal.php">Metal</a></li>
-          <li><a href="all.php">All</a></li>
-        </ul>
-      </li>
-      <li><a href="Purchase.php">Purchase Tickets</a></li>
-      <li><a href="News.php">News</a></li>
-      <li><a href="profile.php">Profile</a></li>
-    </ul>
-  </nav>
 
 <?php
 
